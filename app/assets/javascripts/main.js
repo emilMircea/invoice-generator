4fadafba06f830b2f09805e7d385bf42b2f4f40c
emilMircea <emil.mircea30@gmail.com>
2017-01-31 11:00:39 +0100

    added currency format through accountingjs gem

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,27 @@
+// FORM
+
+let updateTotalPrice = function() {
+    return
+    // calc total price
+    // 1. get all the spans with a class of .pricePerRow and make an array of them
+    // let allPrices = Array.from(document.querySelectorAll('.pricePerRow'))
+    // // 2. make it a numbers array
+    // allPrices = allPrices.map(e => parseInt(e.textContent))
+    // // 3. get sum with reduce
+    //     //var sum = [1, 2, 3].reduce((a, b) => a + b, 0);
+    // let sum = allPrices.reduce((a, b) => a + b, 0);
+    // // 4. put sum into the #subtotal and #invoice_total
+    // if (sum === 0 ) {
+    //     return
+    // }
+    // console.log(sum);
+    // // let subtotal = document.querySelector('#subtotal');
+    // if (isNaN(sum) || null || undefined) {
+    //     return
+    // }
+
+    // subtotal.html.replace(sum);
+    // Create a new text node called "Water"
+    // let textnode = document.createTextNode(sum);
+    //document.querySelector('#subtotal').replace(textnode);
+}
