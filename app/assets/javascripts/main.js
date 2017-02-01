e053fe5ea28c0a5a72e2f1490eb36d0b0f64dc47
emilMircea <emil.mircea30@gmail.com>
2017-02-01 12:29:43 +0100

    setting invoice total amount with vuejs only

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,15 +1,3 @@
 $(function() {
 
-    $('#submit_invoice').on('click', function(){
-        var hiddenField = $('#invoice_total_hidden');
-    	hiddenField.empty();
-        var t = $('#invoice_total').text();
-     	var tNum = Number(t.split('$')[1]);
-    	hiddenField.val(tNum);
-    });
-    // on change function does not work yet
-    // $('#invoice_total').on('change', function () {
-    //     hiddenField.empty();
-    //     hiddenField.val(tNum);
-    // })
 }); // end DOM ready fn
